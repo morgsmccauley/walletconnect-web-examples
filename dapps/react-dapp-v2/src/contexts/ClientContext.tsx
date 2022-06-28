dabeeea1af6bd762be657c0f7c9e838296cc2f44
Ben Kremer <ben@walletconnect.com>
2022-06-28 10:36:16 +0200

    chore: upgrade to beta.101.1 (beta.102 canary)

--- dapps/react-dapp-v2/src/contexts/ClientContext.tsx
+++ dapps/react-dapp-v2/src/contexts/ClientContext.tsx
@@ -19,7 +19,7 @@ import {
   DEFAULT_RELAY_URL,
 } from "../constants";
 import { AccountBalances, apiGetAccountBalance } from "../helpers";
-import { ERROR, getAppMetadata } from "@walletconnect/utils";
+import { getAppMetadata, getSdkError } from "@walletconnect/utils";
 import { getPublicKeysFromAccounts } from "../helpers/solana";
 import { getRequiredNamespaces } from "../helpers/namespaces";
 
@@ -153,7 +153,7 @@ export function ClientContextProvider({ children }: { children: ReactNode | Reac
     }
     await client.disconnect({
       topic: session.topic,
-      reason: ERROR.USER_DISCONNECTED.format(),
+      reason: getSdkError("USER_DISCONNECTED"),
     });
     // Reset app state after disconnect.
     reset();
